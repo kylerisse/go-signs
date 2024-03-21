d05632924061a2ecac47111b098ffff31cca73d6
kylerisse <kylerisse@users.noreply.github.com>
2024-03-20 20:58:46 -0700

    update xml url to 21x

--- signs/config.go
+++ signs/config.go
@@ -13,7 +13,7 @@ type Config struct {
 func NewServerConfig() Config {
 	return Config{
 		Address:           ":8080",
-		ScheduleXMLurl:    "http://www.socallinuxexpo.org/scale/18x/sign.xml",
+		ScheduleXMLurl:    "http://www.socallinuxexpo.org/scale/21x/sign.xml",
 		ScheduleXMLupdate: "@every 5m",
 	}
 }
