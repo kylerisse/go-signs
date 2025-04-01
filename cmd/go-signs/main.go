e9209da0b6a8fa7113e43b25c1447aaf9adcefe7
kylerisse <kylerisse@users.noreply.github.com>
2025-04-01 08:06:27 -0700

    add server config validation

--- cmd/go-signs/main.go
+++ cmd/go-signs/main.go
@@ -17,13 +17,20 @@ func run(c server.Config) error {
 }
 
 func main() {
-	listenPort := flag.String("port", "2017", "Port to listen on")
-	xmlEndpoint := flag.String("xml", "http://www.socallinuxexpo.org/scale/21x/sign.xml", "URL to Drupal XML endpoint")
-	refreshInterval := flag.Int("refresh", 5, "Schedule refresh interval in minutes")
+	listenPort := flag.String("port", "2017", "Port to listen on (1-65535)")
+	xmlEndpoint := flag.String("xml", "http://www.socallinuxexpo.org/scale/21x/sign.xml", "URL to Drupal XML endpoint (must be http or https)")
+	refreshInterval := flag.Int("refresh", 5, "Schedule refresh interval in minutes (minimum 1)")
 	flag.Parse()
 
-	conf := server.NewConfig(*listenPort, *xmlEndpoint, *refreshInterval)
-	err := run(conf)
+	// Create config with validation
+	conf, err := server.NewConfig(*listenPort, *xmlEndpoint, *refreshInterval)
+	if err != nil {
+		// Show usage on validation error
+		flag.Usage()
+		log.Fatal(err)
+	}
+
+	err = run(conf)
 	if err != nil {
 		log.Fatal(err)
 	}
