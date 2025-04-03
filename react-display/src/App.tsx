929b661a42a4391c4e9e082b5ec6ae6a0dc1b82f
kylerisse <kylerisse@users.noreply.github.com>
2025-04-02 18:27:18 -0700

    init react frontend

---
+++ react-display/src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div>
+        <img src="./logo.png" className="logo"/>
+        <img src="./wifi.png" className="wifi"/>
+      </div>
+      <h1>SCaLE Display</h1>
+    </>
+  )
+}
+
+export default App
