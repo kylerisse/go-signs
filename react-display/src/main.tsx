fa3256ab7a624d2f981f67a83b0039a585930d1a
kylerisse <kylerisse@users.noreply.github.com>
2025-04-02 18:59:42 -0700

    fix nullable

--- react-display/src/main.tsx
+++ react-display/src/main.tsx
@@ -3,7 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
-createRoot(document.getElementById('root')!).render(
+const container = document.getElementById('root')
+if (!container) {
+  throw new Error("Root container not found")
+}
+
+createRoot(container).render(
   <StrictMode>
     <App />
   </StrictMode>,
